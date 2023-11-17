255728f70f2dea83f108e10f6de678cc0c3eec13
Romeo John Perez <rumyoonomicon@gmail.com>
2023-11-17 15:36:42 +0800

    Create QuestGUI.client.lua
    - Finished Constructing the GUI design.
    - Adjustments may come afterwards
    - Bind clientsided btns to serverside modules

---
+++ Main/QuestGUI.client.lua
@@ -0,0 +1,21 @@
+-- QuestGUI buttons Client Side
+
+local RS = game:GetService("ReplicatedStorage")
+local TS = game:GetService("TweenService")
+
+local player = game.Players.LocalPlayer
+local char = player.Character or player.CharacterAdded:Wait()
+local playerGui = player.PlayerGui
+
+-- retrieve buttons
+local QuestSystemScreen = playerGui:WaitForChild("QuestSystem")
+
+local questBtn = QuestSystemScreen.MainButton
+
+-- button actions
+questBtn.
+
+-- client side functions to serverside
+
+
+-- returning lists from serverside to clientside
